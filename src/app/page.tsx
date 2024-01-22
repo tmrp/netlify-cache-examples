82b3179a327f00e3196dbb1321578166ea36e69a
Timothy Peterson <tim@t2.gg>
2024-01-22 19:34:22 +0100

    got some kind of search working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { PageWrapper } from '../components/page-wrapper';
+import { api } from '../trpc/server/trpc-api';
 
 export default async function HomePage() {
+  const cached = api.netlify.getCachedBlobs.query();
+
+  console.log('cached', cached);
   return <h1>home</h1>;
 }
