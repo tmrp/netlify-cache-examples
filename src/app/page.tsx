8f714b2814bf0222b1d2bedfafb1bcd00e2d5b93
Timothy Peterson <tim@t2.gg>
2024-01-24 13:37:59 +0100

    init shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from 'components/ui/button';
 import { PageWrapper } from '../components/page-wrapper';
 import { api } from '../trpc/server/trpc-api';
 
 export default async function HomePage() {
-  const cached = api.netlify.getCachedBlobs.query();
-
-  console.log('cached', cached);
-  return <h1>home</h1>;
+  return (
+    <>
+      <Button>button!</Button>
+      <h1>home</h1>
+    </>
+  );
 }
