de623fa04529a43dfd30d2f5a33227b994b7c2ad
Timothy Peterson <tim@t2.gg>
2024-01-24 19:04:48 +0100

    added builder to trpc functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,36 @@
 import { Card } from "components/card";
+import { TypographyH2 } from "components/typography/typography-h2";
+import { TypographyP } from "components/typography/typography-p";
+import { api } from "trpc/server/trpc-api";
 
 export default async function HomePage() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/.netlify/builders/on-demand-builder`,
-  );
-  const { card } = await response.json();
+  const cardData = await api.netlify.onDemand.query();
 
   return (
     <div className="container relative">
-      <h1>home</h1>
-      <div>
-        <ul className="flex flex-row flex-wrap gap-4">
-          {card.data.map((card) => (
-            <li key={card.id}>
-              <Card
-                imageSrc={card.images.small}
-                imageAlt={`${card.name} illustrated playing card`}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
+      {cardData && (
+        <div className="rounded-md bg-blue-200 p-2">
+          <div>
+            <TypographyH2>
+              This response has been made with a Netlify On Demand builder
+              function
+            </TypographyH2>
+            <TypographyP>
+              This data was generated on {cardData.timeStamp}
+            </TypographyP>
+          </div>
+          <ul className="flex flex-row flex-wrap gap-4">
+            {cardData.data.map((card) => (
+              <li key={card.id}>
+                <Card
+                  imageSrc={card.images.small}
+                  imageAlt={`${card.name} illustrated playing card`}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
