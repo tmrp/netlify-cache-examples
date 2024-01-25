323ec859b6501604f827ff88a973aea0babad251
Timothy Peterson <tim@t2.gg>
2024-01-25 14:47:40 +0100

    added cookie cache

---
+++ src/app/cookie/page.tsx
@@ -0,0 +1,36 @@
+import { Card } from "components/card";
+
+import { TypographyH2 } from "components/typography/typography-h2";
+import { TypographyP } from "components/typography/typography-p";
+import { api } from "trpc/server/trpc-api";
+
+export default async function VariedPage() {
+  const cards = await api.pokemon.getRandomPokemonCardsByType.query();
+
+  return (
+    <div className="container relative">
+      {cards && (
+        <div className="rounded-md bg-cyan-200 p-2">
+          <div>
+            <TypographyH2>
+              This response has been made with a Netlify Edge Function
+            </TypographyH2>
+            <TypographyP>
+              This data was generated on {cards.timeStamp}
+            </TypographyP>
+          </div>
+          <ul className="flex flex-row flex-wrap gap-4">
+            {cards.data.map((card) => (
+              <li key={card.id}>
+                <Card
+                  imageSrc={card.images.small}
+                  imageAlt={`${card.name} illustrated playing card`}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
