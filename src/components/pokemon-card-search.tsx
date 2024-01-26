e4080b1bc4bad048cdccfcd95daab58419c8a12d
Timothy Peterson <tim@t2.gg>
2024-01-26 15:00:00 +0100

    some improvements

---
+++ src/components/pokemon-card-search.tsx
@@ -0,0 +1,11 @@
+import { ClearSearchForm } from "./forms/clear-search-from";
+import { SearchForm } from "./forms/search-form";
+
+export const PokeMonCardSearch = () => {
+  return (
+    <>
+      <ClearSearchForm />
+      <SearchForm />
+    </>
+  );
+};
